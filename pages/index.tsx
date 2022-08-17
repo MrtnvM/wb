aeba8f795e0febabc31f182754d71411ff3ec233
Maxim Martynov <max.mrtnv@gmail.com>
2022-08-17 20:58:36 +0400

    Implementation: Scan button + navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-import styles from "../styles/Home.module.css";
 import { Html5QrcodeScanner } from "html5-qrcode";
 import { useEffect } from "react";
+import MainNavbar from "./components/navbar/MainNavbar";
+import { BiBarcodeReader } from "react-icons/bi";
 
 export default function Home() {
   useEffect(() => {
-    const resultElement = document.getElementById("result")!;
+    const resultElement = document.getElementById("result");
+    const readerElement = document.getElementById("reader");
+    if (!resultElement || !readerElement) return;
 
     function onScanSuccess(decodedText: string, decodedResult: any) {
       const result = `Code matched = ${decodedText}<br />Result: ${JSON.stringify(
@@ -12,13 +15,13 @@ export default function Home() {
       )}`;
       console.log(result);
 
-      resultElement.innerHTML = result;
+      resultElement!.innerHTML = result;
     }
 
     function onScanFailure(error: any) {
       const err = `Code scan error = ${error}`;
       console.warn(err);
-      resultElement.innerHTML = err;
+      resultElement!.innerHTML = err;
     }
 
     let html5QrcodeScanner = new Html5QrcodeScanner(
@@ -35,67 +38,31 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <div>
+      <main>
+        <MainNavbar />
 
-        <div
+        {/* <div
           id="reader"
           style={{ width: "100%", marginLeft: 16, marginRight: 16 }}
-        />
-
+        /> */}
         <div
           id="result"
           style={{ margin: 16, border: "1px solid #ddd", padding: 12 }}
         ></div>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <div className="min-h-full"></div>
+
+      <footer>
+        <button className="rounded-full bg-purple-700 px-4 py-2 fixed bottom-8 right-8 drop-shadow-md flex row items-center">
+          <BiBarcodeReader color="white" size={16} className="mr-2" />
+          <span className="text-white" style={{ fontFamily: "Inter" }}>
+            Сканировать
+          </span>
+        </button>
+
+        <div className="h-2 fixed bottom-0 left-0 right-0 bg-purple-700"></div>
       </footer>
     </div>
   );
